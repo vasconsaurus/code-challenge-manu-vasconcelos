ec92ca14db57980dc5d43c802db5caed0445475c
manu vasconcelos <manu.vasconcelos@gmail.com>
2022-04-16 11:21:19 -0300

    start logic behind creating invitation

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,7 +1,6 @@
 class InvitationsController < ApplicationController
   def new
     @invitation = Invitation.new(cycle: Cycle.find(params[:cycle_id]))
-
     respond_to do |format|
       format.html
       format.js
@@ -9,7 +8,22 @@ class InvitationsController < ApplicationController
   end
 
   def create
-    @invitation = Invitation.create(invitation_params)
+    # hash with email string, break string, turn into array
+    email_ary = h[:email].split(" ")
+    # if email array has 2 elements, first is name, second is email
+    if email_ary.count > 1
+      # save instance with name and email
+      @invitation = Invitation.new
+      @invitation.name = email_ary[0]
+      @invitation.email = email_ary[1]
+      @invitation.cycle_id = #????
+    else
+      @invitation = Invitation.new(invitation_params)
+    end
+    # else it only has an email
+
+    @invitation.save
+    # se salvar o veículo? e se não salvar?
   end
 
   private
